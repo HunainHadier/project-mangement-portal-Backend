c56c0cbb6e31d4aeb47dfb5bebe09d6df62272dd
HakamTechSol <alhakamsofts@gmail.com>
2026-02-13 12:07:44 +0500

    updated-code

--- config/db.js
+++ config/db.js
@@ -1,13 +1,14 @@
-// src/db.js
 import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 export const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  host: process.env.DB_HOST || "78.138.98.18",
+  port: Number(process.env.DB_PORT || 3306),
+  user: process.env.DB_USER || "hakam_remote",
+  password: process.env.DB_PASS || "hakam_project",
+  database: process.env.DB_NAME || "hakam_project",
   connectionLimit: 10,
+  connectTimeout: 10000,
 });
